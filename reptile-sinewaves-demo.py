d92317a04065410999bb66f4563c26d6300e4335
jozseflurvig <lurvig.jozsef97@gmail.com>
2022-10-03 13:53:22 +0200

    Added comments

--- reptile-sinewaves-demo.py
+++ reptile-sinewaves-demo.py
@@ -31,14 +31,11 @@ model = nn.Sequential(
     nn.Linear(64, 1),
 )
 
-def gen_task(show):
+def gen_task():
     "Generate a random `sine function` with random `phase` and random `amplitude`."
     phase = rng.uniform(low=0, high=2*np.pi)           # ф = [0,2π[
     ampl = rng.uniform(0.1, 5)                         # A = [0.1,5[
     f_randomsine = lambda x : ampl * np.sin(x + phase) # A ⋅ sin(x + ф)
-    if show:
-        print(f"phase: {phase}")
-        print(f"amp: {ampl}")
     return f_randomsine
 
 def totorch(x):
@@ -46,14 +43,16 @@ def totorch(x):
     return ag.Variable(torch.Tensor(x))
 
 def train_on_batch(x, y):
+    """Computes loss between `y` and `model(x)`. Then calculates gradients and changes model parameters accordingly."""
     x = totorch(x)
     y = totorch(y)
     model.zero_grad() # Sets gradients of all model parameters to zero.
+    # Get model prediction for x (should be: yₚᵣₑ = y = A⋅sin(x+ф))
     ypred = model(x)
     loss = (ypred - y).pow(2).mean() # average of (yₚᵣₑ - y)²
-    loss.backward()
+    loss.backward() # compute gradients + future calls will accumulate gradients into `param.grad`
     for param in model.parameters(): # Iterator over module parameters.
-        param.data -= innerStepSize * param.grad.data
+        param.data -= innerStepSize * param.grad.data # `param.grad` attribute contains the gradients computed 
 
 def predict(x):
     """Returns `model(ag.Variable(torch.Tensor(x))).data.numpy()`."""
@@ -61,46 +60,43 @@ def predict(x):
     return model(x).data.numpy()
 
 # Choose a fixed task and minibatch for visualization
-print("Sine fct to plot:")
-f_plot = gen_task(True)
+f_plot = gen_task()
 xtrain_plot = x_all[rng.choice(len(x_all), size=ntrain)] # training points
 
 # Reptile training loop
 for i in range(n):
     weights_before = deepcopy(model.state_dict())
     # Generate task
-    f = gen_task(False) # in each iteration: generate a random sine wave
+    f = gen_task() # in each iteration: generate a random sine wave
     y_all = f(x_all) # calculate values on the whole domain
     # Do SGD on this task
     inds = rng.permutation(len(x_all)) # randomly shuffled indeces i. e. [0,50[ --- for example {2 17 28 5 ... 25 38}
-    #print(inds)
     for _ in range(innerEpochs):
         for start in range(0, len(x_all), ntrain): # (start, stop, step) --- (0, 50, 10) --> {0 10 20 30 40}
             mbinds = inds[start:start+ntrain] # minibatch indeces
             train_on_batch(x_all[mbinds], y_all[mbinds]) # train on 10 set of x and y pairs
-    # Interpolate between current weights and trained weights from this task
+    # Interpolate between current weights (weights_before) and trained weights (weights_after) from this task
     # I.e. (weights_before - weights_after) is the meta-gradient
     weights_after = model.state_dict()
-    outerStepSize = outerStepSize0 * (1 - i / n) # linear schedule
+    outerStepSize = outerStepSize0 * (1 - i / n) # linear schedule, i.e. (1 - i/n) is ~1 if i=0, and ~0 if i=n
     model.load_state_dict({name : 
         weights_before[name] + (weights_after[name] - weights_before[name]) * outerStepSize 
         for name in weights_before})
-
-    # Periodically plot the results on a particular task and minibatch
-    if plot and i==0 or (i+1) % 1000 == 0:
-    #if plot and i==n-1:
+    # Plot the results on a particular task and minibatch
+    #if plot and (i==0 or (i+1) % 10000 == 0 or i==n-1):
+    if plot and i==n-1:
         plt.cla()
         f = f_plot
         weights_before = deepcopy(model.state_dict()) # save snapshot before evaluation
-        plt.plot(x_all, predict(x_all), label="pred after 0", color=(0,0,1)) #HACK: plot
-        for inneriter in range(32):
+        plt.plot(x_all, predict(x_all), label="pred after 0", color=(0,0,1))
+        for j in range(32):
             train_on_batch(xtrain_plot, f(xtrain_plot))
-            if (inneriter+1) % 8 == 0:
-                frac = (inneriter+1) / 32
-                plt.plot(x_all, predict(x_all), label="pred after %i"%(inneriter+1), color=(frac, 0, 1-frac)) #HACK: plot
-        plt.plot(x_all, f(x_all), label="true", color=(0,1,0)) #HACK: plot
+            if (j + 1) % 8 == 0:
+                frac = (j + 1) / 32
+                plt.plot(x_all, predict(x_all), label="pred after %i"%(j + 1), color=(frac, 0, 1 - frac))
+        plt.plot(x_all, f(x_all), label="true", color=(0,1,0))
         lossval = np.square(predict(x_all) - f(x_all)).mean() # would be better to average loss over a set of examples, but this is optimized for brevity
-        plt.plot(xtrain_plot, f(xtrain_plot), "x", label="train", color="k") #HACK: plot
+        plt.plot(xtrain_plot, f(xtrain_plot), "x", label="train", color="k")
         plt.ylim(-4,4)
         plt.legend(loc="lower right")
         plt.pause(0.01)
