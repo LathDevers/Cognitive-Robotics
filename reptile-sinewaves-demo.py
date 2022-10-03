fe9ad7d8c8fef75cc146a056c57295bea7a034e5
jozseflurvig <lurvig.jozsef97@gmail.com>
2022-10-03 18:36:36 +0200

    No comment

--- reptile-sinewaves-demo.py
+++ reptile-sinewaves-demo.py
@@ -79,7 +79,7 @@ for i in range(n):
     weights_after = model.state_dict()
     outerStepSize = outerStepSize0 * (1 - i / n) # linear schedule, i.e. (1 - i/n) is ~1 if i=0, and ~0 if i=n
     model.load_state_dict({name : 
-        weights_before[name] + (weights_after[name] - weights_before[name]) * outerStepSize 
+        weights_before[name] + (weights_after[name] - weights_before[name]) * outerStepSize
         for name in weights_before})
     # Plot the results on a particular task and minibatch
     #if (i==0 or (i+1) % 10000 == 0 or i==n-1):
@@ -88,11 +88,11 @@ for i in range(n):
         f = f_plot
         weights_before = deepcopy(model.state_dict()) # save snapshot before evaluation
         plt.plot(x_all, predict(x_all), label="pred after 0", color=(0,0,1))
-        for j in range(32):
-            train_on_batch(xtrain_plot, f(xtrain_plot))
-            if (j + 1) % 8 == 0:
-                frac = (j + 1) / 32
-                plt.plot(x_all, predict(x_all), label="pred after %i"%(j + 1), color=(frac, 0, 1 - frac))
+        #for j in range(32):
+        #    train_on_batch(xtrain_plot, f(xtrain_plot))
+        #    if (j + 1) % 8 == 0:
+        #        frac = (j + 1) / 32
+        #        plt.plot(x_all, predict(x_all), label="pred after %i"%(j + 1), color=(frac, 0, 1 - frac))
         plt.plot(x_all, f(x_all), label="true", color=(0,1,0))
         lossval = np.square(predict(x_all) - f(x_all)).mean() # would be better to average loss over a set of examples, but this is optimized for brevity
         plt.plot(xtrain_plot, f(xtrain_plot), "x", label="train", color="k")
